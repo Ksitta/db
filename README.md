9d648b9f9b903e6d6bc5e85992274918e92638af
frw <fruwen@163.com>
2022-11-18 16:22:16 +0800

    add table pk fk related func

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 2022 秋季学期《数据库系统概论大作业》。
 
 ## 安装依赖
+
 使用python实现，python 3.10.6
 使用antlr4-4.7.2
 antlr非必须，只需要有运行时即可
+
 ```bash
 sudo apt install antlr4=4.7.2-5 # 非必须
 ```
--- src/table/table.py
+++ src/table/table.py
@@ -3,40 +3,43 @@ from record_management.rm_file_handle import RM_FileHandle
 from table.record import Record, Column
 import numpy as np
 class Table():
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, columns: list, pk: list, fk: dict) -> None:
         self._name: str = name
-        self._file_handle: RM_FileHandle = rm_manager.open_file(name)
-        meta: dict = self._file_handle.read_meta()
-        self._columns: list = list()
-        for each in meta['columns']:
-            column = Column(each)
-            self._columns.append(column)
-
-    def __init__(self, name: str, columns: list, pk: dict, fk: dict) -> None:
-        self._name = name
-        
-        rm_manager.create_file(name)
-        self._file_handle: RM_FileHandle = rm_manager.open_file(name)
-        self._columns = columns
-        
-        meta: dict = dict()
-        meta['column_number'] = len(columns)
-        
-        record_size: int = 0
-        columns: list = list()
-        
-        for i in columns:
-            each: Column = i
-            column: dict = dict()
-            column['column_type'] = each.type
-            column['column_size'] = each.size
-            column['column_name_length'] = len(each.name)
-            column['column_name'] = each.name
-            columns.append(column)
-            record_size += len(each.name)
-        meta['columns'] = columns
-        meta['record_size'] = record_size
-        self._file_handle.init_meta(meta)
+        if columns == None:
+            self._file_handle: RM_FileHandle = rm_manager.open_file(name)
+            meta: dict = self._file_handle.read_meta()
+            self._columns: list = list()
+            for each in meta['columns']:
+                column = Column(each)
+                self._columns.append(column)
+        else:
+            rm_manager.create_file(name)
+            self._file_handle: RM_FileHandle = rm_manager.open_file(name)
+            self._columns = columns
+            
+            meta: dict = dict()
+            meta['column_number'] = len(columns)
+            
+            record_size: int = 0
+            columns: list = list()
+            
+            for i in columns:
+                each: Column = i
+                column: dict = dict()
+                column['column_type'] = each.type
+                column['column_size'] = each.size
+                column['column_name_length'] = len(each.name)
+                column['column_name'] = each.name
+                columns.append(column)
+                record_size += len(each.name)
+            meta['columns'] = columns
+            meta['record_size'] = record_size
+            meta['primary_key_size'] = len(pk)
+            meta['primary_keys'] = []
+            # meta['foreign_key_number'] = len(fk)
+            
+            self._file_handle.init_meta(meta)
+   
 
     def __del__(self):
         if self._file_handle:
@@ -47,14 +50,14 @@ class Table():
         self._file_handle = None
 
     def insert_record(self, record: Record):
-        data: np.darray = record.to_nparray(self._columns)
+        data: np.ndarray = record.to_nparray(self._columns)
         self._file_handle.insert_record(data)
     
     def delete_record(self, rid):
         self._file_handle.remove_record(rid)
     
     def update_record(self, rid, record):
-        data = record.to_nparray(self._columns)
+        data: np.ndarray = record.to_nparray(self._columns)
         self._file_handle.update_record(rid, data)
         
     
